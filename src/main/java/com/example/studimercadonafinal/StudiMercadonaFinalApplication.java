8374695e8a5bf3669ee74aa2990e2b2125733e1c
hugo-rgr <hugo@silk-digital.com>
2023-11-13 19:00:12 +0100

    Init

---
+++ src/main/java/com/example/studimercadonafinal/StudiMercadonaFinalApplication.java
@@ -0,0 +1,16 @@
+package com.example.studimercadonafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class StudiMercadonaFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiMercadonaFinalApplication.class, args);
+    }
+
+}

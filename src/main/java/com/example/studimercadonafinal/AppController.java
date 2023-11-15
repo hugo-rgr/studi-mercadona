9caa7820afb0e83b8e1400fc9bab517d071089d0
hugo-rgr <hugo@silk-digital.com>
2023-11-16 00:56:29 +0100

    Removed test route

--- src/main/java/com/example/studimercadonafinal/AppController.java
+++ src/main/java/com/example/studimercadonafinal/AppController.java
@@ -6,12 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
-    @RequestMapping("/")
-    public String index(Model model) {
-        model.addAttribute("message", "Hey!!!!");
-        return "greeting";
-    }
-
     @GetMapping("/admin-dashboard")
     public String adminDashboard() {
         return "admin-dashboard";

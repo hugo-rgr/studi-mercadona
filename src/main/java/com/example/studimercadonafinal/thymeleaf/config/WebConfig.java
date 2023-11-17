ea143d368c6528d6ab76b9b41b25077b2039703f
hugo-rgr <hugo@silk-digital.com>
2023-11-17 09:38:29 +0100

    Move WebConfig

--- src/main/java/com/example/studimercadonafinal/thymeleaf/config/WebConfig.java
+++ src/main/java/com/example/studimercadonafinal/thymeleaf/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.studimercadonafinal.config;
+package com.example.studimercadonafinal.thymeleaf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

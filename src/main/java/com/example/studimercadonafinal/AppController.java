708e3667206cd75d5b4dcee207b350daae283511
hugo-rgr <hugo@silk-digital.com>
2023-11-17 09:38:16 +0100

    Update catalog and login pages + AppController

--- src/main/java/com/example/studimercadonafinal/AppController.java
+++ src/main/java/com/example/studimercadonafinal/AppController.java
@@ -1,8 +1,6 @@
 package com.example.studimercadonafinal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
